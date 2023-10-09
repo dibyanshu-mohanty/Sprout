62ef9b1ffc7694a4b6bdd686ed30b9d1c2fd8c04
dibydev <dibyanshu.mohanty@momoney.in>
2023-10-09 16:28:27 +0530

    Initial Commit

---
+++ Sprout/SproutApp.swift
@@ -0,0 +1,17 @@
+//
+//  SproutApp.swift
+//  Sprout
+//
+//  Created by Dibyanshu Mohanty on 09/10/23.
+//
+
+import SwiftUI
+
+@main
+struct SproutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
